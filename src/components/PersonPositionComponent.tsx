a92bc4923baa07f5d76dc0252c4867620e3520f7
Alice Wu <alwu5269@gmail.com>
2018-12-05 16:12:51 -0800

    Fixed most right element hover so now within window borders.

--- src/components/PersonPositionComponent.tsx
+++ src/components/PersonPositionComponent.tsx
@@ -1,3 +1,4 @@
+import { HoverWrapperComponent } from "src/components/HoverWrapperComponent";
 import * as React from "react";
 import { PersonComponent } from "src/components/PersonComponent";
 import * as AFX from "src/data/data";
@@ -12,28 +13,28 @@ export class PersonPositionComponent extends React.Component<any> {
 
     if (personPosition.picture) {
       return (
-        <div className="board--member">
-          <img
-            className="board--img"
-            src={personPosition.picture[0].thumbnails.large.url}
-            width="200px"
-            height="200px"
-          />
-          <div className="overlay">
-            <div className="member-info">
-              {personComp}
-              <p className="title">{personPosition.positionTitle}</p>
-            </div>
-          </div>
-        </div>
-        /*
+        // <div className="board--member">
+        //   <img
+        //     className="board--img"
+        //     src={personPosition.picture[0].thumbnails.large.url}
+        //     width="200px"
+        //     height="200px"
+        //   />
+        //   <div className="overlay">
+        //     <div className="member-info">
+        //       {personComp}
+        //       <p className="title">{personPosition.positionTitle}</p>
+        //     </div>
+        //   </div>
+        // </div>
+
         <HoverWrapperComponent pp={personPosition}>
           <div className="board--member">
             <img
               className="board--img"
               src={personPosition.picture[0].url}
-              width="200px"
-              height="200px"
+              width="50px"
+              height="50px"
             />
             <div className="overlay">
               <div className="member-info">
@@ -43,32 +44,31 @@ export class PersonPositionComponent extends React.Component<any> {
             </div>
           </div>
         </HoverWrapperComponent>
-        */
       );
     } else {
       return (
-        <div className="board--member">
-          <img
-            className="board--img"
-            src={"https://i.imgur.com/4qcFzdm.png"}
-            width="200px"
-            height="200px"
-          />
-          <div className="overlay">
-            <div className="member-info">
-              {personComp}
-              <p className="title">{personPosition.positionTitle}</p>
-            </div>
-          </div>
-        </div>
-        /*
+        // <div className="board--member">
+        //   <img
+        //     className="board--img"
+        //     src={"https://i.imgur.com/4qcFzdm.png"}
+        //     width="200px"
+        //     height="200px"
+        //   />
+        //   <div className="overlay">
+        //     <div className="member-info">
+        //       {personComp}
+        //       <p className="title">{personPosition.positionTitle}</p>
+        //     </div>
+        //   </div>
+        // </div>
+
         <HoverWrapperComponent pp={personPosition}>
           <div className="board--member">
             <img
               className="board--img"
               src={"https://i.imgur.com/4qcFzdm.png"}
-              width="200px"
-              height="200px"
+              width="50px"
+              height="50px"
             />
             <div className="overlay">
               <div className="member-info">
@@ -78,7 +78,6 @@ export class PersonPositionComponent extends React.Component<any> {
             </div>
           </div>
         </HoverWrapperComponent>
-        */
       );
     }
   }
