076b51634c57a4f2c01afc82a8a6c67f8eb67a69
Catherine Chi <catherinechi@Catherines-MacBook-Pro-2.local>
2019-05-22 10:18:12 -0700

    spring 2019 release

---
+++ docs/precache-manifest.751046a51cac4678d6f6ca8540902bb0.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "229c360febb4351a89df",
+    "url": "/static/js/runtime~main.229c360f.js"
+  },
+  {
+    "revision": "0a0a7b3b150de5aff56c",
+    "url": "/static/js/main.0a0a7b3b.chunk.js"
+  },
+  {
+    "revision": "8602115c589276779fff",
+    "url": "/static/js/1.8602115c.chunk.js"
+  },
+  {
+    "revision": "0a0a7b3b150de5aff56c",
+    "url": "/static/css/main.870adc49.chunk.css"
+  },
+  {
+    "revision": "9d92fa7118d98ef813abd6be1baa8921",
+    "url": "/index.html"
+  }
+];
