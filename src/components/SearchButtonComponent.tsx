ad37d2ef9d4f4adb812493f9c45acdd9c744895f
Charles Wang <charleswang@Charless-MacBook-Air.local>
2019-11-15 12:58:29 -0800

    made search buttons clickable

--- src/components/SearchButtonComponent.tsx
+++ src/components/SearchButtonComponent.tsx
@@ -8,13 +8,6 @@ export class SearchButtonComponent extends React.Component<{ name: string }> {
       <div className="search-button">
         <a href={redirectLink}>{name}</a>
       </div>
-      // <div>
-      //   <form action="/persons" method="get">
-      //     <input type="hidden" name="name" value={name}></input>
-
-      //     <button className="nameButton" type="submit">{name}</button>
-      //   </form>
-      // </div>
 
     );
   }
