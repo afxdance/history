7a0a8f20c1fbf0f96168dee98e8b0a900b98db80
Miles Wada <miles_wada@berkeley.edu>
2020-04-05 17:34:38 -0700

    updated loadfb

--- src/components/EventsComponent.tsx
+++ src/components/EventsComponent.tsx
@@ -18,6 +18,10 @@ export class EventsComponent extends React.Component {
   constructor(props: any) {
     super(props);
 
+    this.setState({
+      events: []
+    });
+    this.loadFbApi();
   }
 
   // public loadFbApi() {
@@ -30,6 +34,23 @@ export class EventsComponent extends React.Component {
   // public componentDidMount() {
   //   this.loadFbApi();
   // }
+
+  public loadFbApi() {
+    const axios = require("axios");
+    const page_id = "100384541604731";
+    const access_token = "EAAW3scDWJpwBAJsrWiDppdBQ0eYte1m8oR7BOCRBAInpiZAY7uFIBdea6p8IZBU5PLkQ8koKAhz27mGQeAY067aBtgUifDyu2L1HRm3DAuSvhjo6bXnVLJacnzZA78HOkogtfDspGcuNHwrSGuojlqUcD5SZCuZB5X1bLo0nps8hbZCKZC9V7nGhigDZBHbQ59ucdYaZC5P2E2wZDZD";
+
+    /** We need to convert the short lived access_token above into a long lived access token,
+     * and then convert that into a never-expiring page access token
+    */
+
+    axios.get("https://graph.facebook.com/" + page_id + "/events?access_token=" + access_token)
+      .then(response => {
+        console.log(response);
+        var eventsList: any[] = []
+
+      });
+  }
 
   public render() {
 
