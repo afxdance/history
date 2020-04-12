8185929d03d874c7f60abc6aaf9d3012e2cab377
Miles Wada <miles_wada@berkeley.edu>
2020-04-12 16:13:11 -0700

    events now show up on calendar

--- src/components/EventsComponent.tsx
+++ src/components/EventsComponent.tsx
@@ -7,60 +7,70 @@ import "react-big-calendar/lib/css/react-big-calendar.css";
 import 'react-big-calendar/lib/sass/styles.scss';
 
 
-declare global {
-  interface Window {
-    FB: any;
-    fbAsyncInit: any;
-  }
+type EventsList = {
+  events: any[]
 }
 
-export class EventsComponent extends React.Component {
+
+export class EventsComponent extends React.Component<{}, EventsList> {
   constructor(props: any) {
     super(props);
 
-    this.setState({
-      events: []
-    });
-    this.loadFbApi();
+    this.state = {
+      events: [],
+    };
   }
 
-  // public loadFbApi() {
-  //   window.FB.api("/me", "GET", { fields: "id,name" }, function(response: any) {
-  //     // Insert your code here
-  //     console.log(response);
-  //   });
-  // }
-
-  // public componentDidMount() {
-  //   this.loadFbApi();
-  // }
 
   public loadFbApi() {
     const axios = require("axios");
     const page_id = "100384541604731";
-    const access_token = "EAAW3scDWJpwBAJsrWiDppdBQ0eYte1m8oR7BOCRBAInpiZAY7uFIBdea6p8IZBU5PLkQ8koKAhz27mGQeAY067aBtgUifDyu2L1HRm3DAuSvhjo6bXnVLJacnzZA78HOkogtfDspGcuNHwrSGuojlqUcD5SZCuZB5X1bLo0nps8hbZCKZC9V7nGhigDZBHbQ59ucdYaZC5P2E2wZDZD";
+    const access_token = "EAAW3scDWJpwBAHDKW9gZAQDRZAo8B09bWH7qWUl0whL7t2ZBL1FrX0YXmmWCRfZCMNpmaNrzP0hR4YKAw80cWm0tyR6WXBKTONdYgP6UYnlACaTgROwp5BYZBmlZCQkBRuMq8tB0X0sqZAdKY0C6X5BkRzSJ3Y8rFhw7TirtSwuBUMu0Bh2mTnflUW9abZBvqffOWPH0aYKRqwZDZD";
 
     /** We need to convert the short lived access_token above into a long lived access token,
-     * and then convert that into a never-expiring page access token
+     * and then convert that into a never-expiring page access token.
     */
 
     axios.get("https://graph.facebook.com/" + page_id + "/events?access_token=" + access_token)
       .then(response => {
-        console.log(response);
-        var eventsList: any[] = []
+        var eventsList: any[] = [];
 
+        var count = 0;
+        for (let events in response["data"]["data"]) {
+
+          var tempEvent = response["data"]["data"][events];
+
+          console.log(tempEvent);
+
+          eventsList.push(
+            {
+              id: count,
+              title: tempEvent["description"],
+              start: new Date(tempEvent["start_time"]),
+              end: new Date(tempEvent["end_time"])
+
+            }
+          );
+          count += 1;
+        }
+
+        /** Extract relevant information from eventsList. */
+        this.setState({ events: eventsList });
       });
   }
 
-  public render() {
+  public componentWillMount() {
+    this.loadFbApi();
+  }
 
+  public render() {
     const localizer = momentLocalizer(moment);
 
     return (
       <div>
         <Calendar
           localizer={localizer}
-          events={[]}
+          events={this.state.events}
           startAccessor="start"
           endAcessor="end"
           style={{ height: 800 }}
