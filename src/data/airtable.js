6794470516f22f00dd4d75562640e91f244ba069
Ethan Pang <ethanp1138@gmail.com>
2021-10-17 21:00:13 -0700

    first commit

---
+++ src/data/airtable.js
@@ -0,0 +1,3 @@
+// testing script to try automatic airtable api calls
+// can call fetch-data.js upon loading the website
+// dynamically import src/data/data into each component once the asynchronous call finishes
