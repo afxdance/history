0cc38b1fc7755e031d25c0febf2360fd7eeafa61
Ethan Pang <ethanp1138@gmail.com>
2021-10-24 21:01:44 -0700

    skeleton in airtable.js

--- src/data/airtable.js
+++ src/data/airtable.js
@@ -1,3 +1,37 @@
 // testing script to try automatic airtable api calls
 // can call fetch-data.js upon loading the website
 // dynamically import src/data/data into each component once the asynchronous call finishes
+
+var Airtable = require('airtable');
+var base = new Airtable({ apiKey: 'YOUR_API_KEY' }).base('app5gc254OPlaTFG2');
+
+
+// 1. use semester id from HisotryNavComponent to get the Semesters Table row
+// 2. get the boardGroupId/teamGroupIds field from that semester
+// 3. get the relevant rows from the Groups Table
+
+// Semesters[groupId] -> Groups[positionIds] -> PersonPositions[personIds] -> People[name]
+//                        |_name, teamPicture     |_positionTitle, picture      |_name
+
+let semesterName = '2020a';
+
+base('Semesters').select({
+  view: 'Default View',
+  filterByFormula: "{codename} = '" + { semesterName } + "'"
+}).firstPage(function (err, records) {
+  if (err) { console.error(err); return; }
+  records.forEach(function (record) {
+    console.log('Retrieved', record.get('codename'));
+
+    groupId = record.get('boardGroupId')[0];
+
+    base('Groups').find(groupId, function (err2, groupRecord) {
+      if (err2) { console.error(err2); return; }
+      console.log('Retrieved', groupRecord.id);
+
+      // write data into a [people]_data.tsx file / local storage
+    });
+
+
+  });
+});
