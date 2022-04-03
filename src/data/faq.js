e93830c7b4a0985adde158a3430693ed6c4aa096
Chloe Zhong <czhongx4@berkeley.edu>
2022-04-03 16:19:03 -0700

    merge

---
+++ src/data/faq.js
@@ -0,0 +1,32 @@
+export const faqs = [
+  {
+    title: 'What is AFX Gen?',
+    content: 'AFX is the only dance organization on campus that has been committed to accepting everyone as long as you do an audition! This occurs mainly through AFX Gen.'
+  },
+  {
+    title: 'What are the different AFX Gen teams?',
+    content: 'There are two different AFX Gen teams: training (beginner) and project (intermediate).'
+  },
+  {
+    title: 'What is the difference between AFX Training and AFX Project?',
+    content: 'In addition to difference in time commitment (project teams will require a bit more time), AFX Project choreography is usually a bit more fast-paced than AFX Training choreography.'
+  },
+  {
+    title: 'What is the audition process like to join AFX?',
+    content: 'Everyone is welcome to audition for AFX at the beginning of each semester, where you will audition for the Training and Project Team in the same audition! A auditions, you will learn a set taught by our directors. You will then perform this set in groups to the committee of directors along with a couple of seconds of freestyle.'
+  },
+  {
+    title: 'What if I’m too nervous/scared to dance and look bad?',
+    content: 'AFX is a safe space that helps you secure a spot on a dance team and learn! Many of our dancers (and even directors) have never danced before college or even before joining AFX.'
+  },
+  {
+    title: 'What does the time commitment look like for AFX Gen?',
+    content: 'Training: 5 to 6 hours \nProject: 6 to 8 hours \nThis consists of team practices and socials. You will be working towards a set with your directors for the End-of-Semester Showcase!',
+  },
+  {
+    title: '',
+    content: ''
+  }
+
+
+];
