928cce69cea9e4f2de10ea1b41d0e97e8ec26d63
Justin Xia <justinerxiabucks0119@berkeley.edu>
2022-05-06 19:17:02 -0700

    events schedule done

--- src/components/EventsComponent.tsx
+++ src/components/EventsComponent.tsx
@@ -1,12 +1,20 @@
-import * as React from "react";
-import { Calendar, momentLocalizer } from "react-big-calendar";
-import { TeamSchedule, days, dayType, singleTeam, teamScheduleType } from './helperComponents/teamSchedule';
-// @ts-ignore
-import moment from "moment";
-import "react-big-calendar/lib/css/react-big-calendar.css";
-import "react-big-calendar/lib/sass/styles.scss";
+import * as React from "react"
+import { useState } from "react"
+import { Calendar, momentLocalizer } from "react-big-calendar"
+import {
+  TeamSchedule,
+  days,
+  singleTeam,
+} from "./helperComponents/teamSchedule"
 
-const localizer = momentLocalizer(moment);
+// @ts-ignore
+import moment from "moment"
+import "react-big-calendar/lib/css/react-big-calendar.css"
+import "react-big-calendar/lib/sass/styles.scss"
+// import "instafeed.js"
+import InstagramEmbed from 'react-instagram-embed';
+
+const localizer = momentLocalizer(moment)
 
 /**
  * A component that renders a React Big Calendar that displays events posted on
@@ -15,21 +23,26 @@ const localizer = momentLocalizer(moment);
  * Displays events according to their title and starting/ending time.
  *  */
 export const EventsComponent: React.FC = () => {
-  const emptyList: any[] = [];
-  const [events, setEvents] = React.useState(emptyList);
+  const emptyList: any[] = []
+  const [events, setEvents] = React.useState(emptyList)
+
 
   //console.log(process.env);
   const calendar_id = "rguja84c2kdipbuf3k866btq50@group.calendar.google.com"
 
   React.useEffect(() => {
-    const axios = require("axios");
-    axios.get("https://www.googleapis.com/calendar/v3/calendars/" + calendar_id
-      // + "/events?orderBy=startTime&showDeleted=false&singleEvents=true&timeMin=2016-02-05T00%3A00%3A00Z&key=" + process.env.REACT_APP_GCAL_KEY)
-      + "/events?orderBy=startTime&showDeleted=false&singleEvents=true&timeMin=2016-02-05T00%3A00%3A00Z&key=" + "AIzaSyAZvqWSX3sYUtycgABvso6sQAkP137i59M")
-      .then(response => {
-
-        let eventsList: any[] = [];
-        let eventsCount = 0;
+    const axios = require("axios")
+    axios
+      .get(
+        "https://www.googleapis.com/calendar/v3/calendars/" +
+          calendar_id +
+          // + "/events?orderBy=startTime&showDeleted=false&singleEvents=true&timeMin=2016-02-05T00%3A00%3A00Z&key=" + process.env.REACT_APP_GCAL_KEY)
+          "/events?orderBy=startTime&showDeleted=false&singleEvents=true&timeMin=2016-02-05T00%3A00%3A00Z&key=" +
+          "AIzaSyAZvqWSX3sYUtycgABvso6sQAkP137i59M"
+      )
+      .then((response) => {
+        let eventsList: any[] = []
+        let eventsCount = 0
 
         for (let events of response["data"]["items"]) {
           // Check to see if the event has a time
@@ -40,9 +53,9 @@ export const EventsComponent: React.FC = () => {
               start: new Date(events["start"]["dateTime"]),
               end: new Date(events["end"]["dateTime"]),
               allDay: false,
-              link: events["htmlLink"]
-            });
-            eventsCount += 1;
+              link: events["htmlLink"],
+            })
+            eventsCount += 1
 
             // If not, assume it is an all day event
           } else {
@@ -52,37 +65,114 @@ export const EventsComponent: React.FC = () => {
               start: new Date(events["end"]["date"]),
               end: new Date(events["end"]["date"]),
               allDay: true,
-              link: events["htmlLink"]
-            });
+              link: events["htmlLink"],
+            })
           }
         }
-        setEvents(eventsList);
-      });
+        setEvents(eventsList)
+      })
   }, [])
 
   // Monday
   const monday = []
 
   // Tuesday
-  const teamAwayFX: singleTeam = {name: "awayFX", start: "7:00 PM", end: "9:00 PM", loc: "underhill/lower sproul"};
-  const tuesday: singleTeam[] = [teamAwayFX]
+  const awayT: singleTeam = {
+    name: "awayFX",
+    start: "7:00 PM",
+    end: "9:00 PM",
+    loc: "Underhill/Lower Sproul",
+    color: "#ffcccc"
+  }
+  const tuesday: singleTeam[] = [awayT]
 
-  const link: singleTeam = { name: "link", start: "6:30 PM", end: "9:30 PM", loc: "haas pavillion"};
-  const party: singleTeam = { name: "party", start: "6:30 PM", end: "9:00 PM", loc: "lower sproul "};
-  const wednesday: singleTeam[] = [link, party]
-
+  // Wednesday
+  const linkW: singleTeam = {
+    name: "AFX Link",
+    start: "6:30 PM",
+    end: "9:30 PM",
+    loc: "Haas Pavillion",
+    color: "#FEFFCC"
+  }
+  const partyW: singleTeam = {
+    name: "AFX Party!",
+    start: "6:30 PM",
+    end: "9:00 PM",
+    loc: "Lower Sproul",
+    color: "#CCF3FF"
+  }
+  const wednesday: singleTeam[] = [linkW, partyW]
 
   // Thursday
+  const yinyangT: singleTeam = {
+    name: "AFX Yin Yang",
+    start: "7:00 PM",
+    end: "10:00 PM",
+    loc: "Lower Sproul",
+    color: "#CCD4FF"
+  }
+  const thursday: singleTeam[] = [yinyangT]
 
-  const thursday = []
+  // Friday
+  const partyF: singleTeam = {
+    name: "AFX Party!",
+    start: "6:30 PM",
+    end: "9:00 PM",
+    loc: "Lower Sproul",
+    color: "#CCF3FF"
+  }
+  const underscoreF: singleTeam = {
+    name: "AFX ___",
+    start: "7:00 PM",
+    end: "10:00 PM",
+    loc: "Haas Pavillion",
+    color: "#CCFFDA"
+  }
+  const friday: singleTeam[] = [partyF, underscoreF]
 
-  const friday = []
+  // Saturday
+  const awayS: singleTeam = {
+    name: "awayFX",
+    start: "3:30 PM",
+    end: "6:00 PM",
+    loc: "Haas Pavillion",
+    color: "#ffcccc"
+  }
+  const linkS: singleTeam = {
+    name: "AFX Link",
+    start: "6:30 PM",
+    end: "9:30 PM",
+    loc: "Haas Pavillion",
+    color: "#FEFFCC"
+  }
+  const saturday: singleTeam[] = [awayS, linkS]
 
-  const saturday = []
+  // Sunday
+  const underscoreS: singleTeam = {
+    name: "AFX ___",
+    start: "5:00 PM",
+    end: "8:00 PM",
+    loc: "Lower Sproul",
+    color: "#CCFFDA"
+  }
+  const yinyangS: singleTeam = {
+    name: "AFX Yin Yang",
+    start: "7:00 PM",
+    end: "10:00 PM",
+    loc: "Lower Sproul",
+    color: "#CCD4FF"
+  }
+  const sunday: singleTeam[] = [underscoreS, yinyangS]
 
-  const sunday = []
-
-  const Allteams: singleTeam[][]= [monday, tuesday, wednesday, thursday, friday, saturday, sunday];
+  const Allteams: singleTeam[][] = [
+    monday,
+    tuesday,
+    wednesday,
+    thursday,
+    friday,
+    saturday,
+    sunday,
+  ]
 
 
   let teamSchedules = days.map((day, index) => (
@@ -91,7 +181,8 @@ export const EventsComponent: React.FC = () => {
 
   return (
     <div>
-      <div className='name-time-container'>{teamSchedules}</div>
+      <h1>Team Schedule Spring 2022</h1>
+      <div className="name-time-container">{teamSchedules}</div>
       {/* <p>Official calendar TBD! Please go to <a href="https://www.facebook.com/AFXdance">our Facebook page </a>for more details on any upcoming events :)</p> */}
       <Calendar
         localizer={localizer}
@@ -99,12 +190,36 @@ export const EventsComponent: React.FC = () => {
         style={{ height: 800 }}
         onSelectEvent={handleSelectEvent}
       />
+
+      {/* <script type="text/javascript" src="path/to/instafeed.min.js">
+        <div id="instafeed">
+          var feed = new Instafeed({
+              accessToken: 'IGQVJXY2FMal92TUdDako0YW5yUnJKTGRqcjVja2pXRkZAxWnhaU2NpZAmV6dGJjUzVOak9vWGU0VEFuZAjFIandOMkgtWVdlSHpvSVdJdUViRjU0WUlQUVZAIZATlXT2ZAieDZAFbHBDMVRNbjVxblhnX3UySwZDZD'
+          });
+          feed.run();
+        </div>
+      </script> */}
+
+
+      <InstagramEmbed
+        url='https://www.instagram.com/p/Cc9KZQzJnf5/'
+        clientAccessToken=''
+        maxWidth={320}
+        hideCaption={false}
+        containerTagName='div'
+        protocol=''
+        injectScript
+        onLoading={() => {}}
+        onSuccess={() => {}}
+        onAfterRender={() => {}}
+        onFailure={() => {}}
+      />
     </div>
-  );
+  )
 }
 /** Event-handling function that is called when a user
  * clicks on an event.
  */
 function handleSelectEvent(event: object) {
-  window.open(event["link"], "_blank");
+  window.open(event["link"], "_blank")
 }
--- src/components/helperComponents/teamSchedule.tsx
+++ src/components/helperComponents/teamSchedule.tsx
@@ -1,43 +1,42 @@
-import * as React from 'react'; // https://stackoverflow.com/a/56833596/13745526, cannot use import React from 'react' because of type issues
-
+import * as React from "react" // https://stackoverflow.com/a/56833596/13745526, cannot use import React from 'react' because of type issues
 
 export const TeamSchedule: React.FC<teamScheduleType> = ({ day, teams }) => {
 
-  let teamsComponent = teams.map((singleTeam) => (
-      <div>
-        <div style={nameColorBox}>
-          {singleTeam.name}
-        </div>
+  const teamsComponent = teams.map(function (singleTeam) {
+
+    return (<div className="name-time-loc-container">
+      <div className="time-name" style={{backgroundColor: singleTeam.color}}>{singleTeam.name}</div>
+      <div className="time-loc-container">
         <div>
-          <p>{singleTeam.start} - {singleTeam.end}</p>
-          <p>{singleTeam.loc}</p>
+          {singleTeam.start} - {singleTeam.end}
         </div>
-      </div>));
+        <div className="loc-font">{singleTeam.loc}</div>
+      </div>
+    </div>
+    )});
 
   return (
-    <div className='team-schedule-gray-box'>
-      <p>{day}</p>
-      {teamsComponent}
+    <div className="team-schedule-gray-box">
+      <div>
+        <b className="day-font">{day}</b>
+        {teamsComponent}
+      </div>
     </div>
   )
-
 }
 
-export const days = ["Mon", "Tue", "Wed", "Thur", "Fri", "Sat", "Sun"] as const;
+export const days = ["Mon", "Tue", "Wed", "Thur", "Fri", "Sat", "Sun"] as const
 export type dayType = typeof days[number]
 
 export interface teamScheduleType {
-  day: dayType,
+  day: dayType
   teams: singleTeam[]
 }
 
 export interface singleTeam {
-  name: string,
-  start: string,
-  end: string,
+  name: string
+  start: string
+  end: string
   loc: string
-}
-
-const nameColorBox = {
-  'color': "red" // use ReactContext
+  color: string
 }
